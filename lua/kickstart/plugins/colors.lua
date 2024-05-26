65db171866314c8be1f06132d49943ac882d6beb
Marin Peric Radan <marin.peric@seekandhit.com>
2024-05-26 19:19:30 +0200

    Additional updates to color.lua file

--- lua/kickstart/plugins/colors.lua
+++ lua/kickstart/plugins/colors.lua
@@ -1,22 +1,71 @@
+-- return {
+--   'folke/tokyonight.nvim',
+--   --[[ 'rose-pine/neovim', ]]
+--   priority = 1000,
+--   config = function()
+--     local transparent = true -- set to true if you would like to enable transparency
+--
+--     --require('rose-pine').setup {
+--     require('tokyonight').setup {
+--       --variant = 'moon', -- Variant options are main, moon, dawn
+--       style = 'night', -- the theme comes in three styles: `storm`, `moon`, a darker variant `night`, and `day`
+--       transparent = transparent,
+--       styles = {
+--         sidebars = transparent and 'transparent' or 'dark',
+--         floats = transparent and 'transparent' or 'dark',
+--       },
+--     }
+--
+--     vim.cmd 'colorscheme tokyonight'
+--     --vim.cmd 'colorscheme rose-pine'
+--   end,
+-- }
+
+function ColorMyPencils(color)
+  color = color or 'rose-pine'
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
 return {
-  'folke/tokyonight.nvim',
-  --[[ 'rose-pine/neovim', ]]
-  priority = 1000,
-  config = function()
-    local transparent = true -- set to true if you would like to enable transparency
+  {
+    'folke/tokyonight.nvim',
+    config = function()
+      require('tokyonight').setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+        transparent = true, -- Enable this to disable setting the background color
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          -- Background styles. Can be "dark", "transparent" or "normal"
+          sidebars = 'dark', -- style for sidebars, see below
+          floats = 'dark', -- style for floating windows
+        },
+      }
+    end,
+  },
 
-    --require('rose-pine').setup {
-    require('tokyonight').setup {
-      --variant = 'moon', -- Variant options are main, moon, dawn
-      style = 'night', -- the theme comes in three styles: `storm`, `moon`, a darker variant `night`, and `day`
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and 'transparent' or 'dark',
-        floats = transparent and 'transparent' or 'dark',
-      },
-    }
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      require('rose-pine').setup {
+        disable_background = true,
+        styles = {
+          italic = false,
+        },
+      }
 
-    vim.cmd 'colorscheme tokyonight'
-    --vim.cmd 'colorscheme rose-pine'
-  end,
+      vim.cmd 'colorscheme rose-pine'
+
+      ColorMyPencils()
+    end,
+  },
 }
